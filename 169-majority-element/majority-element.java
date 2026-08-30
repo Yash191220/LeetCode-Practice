e867b7ec289252772cbf3388f2051e2e92840a30
Yashraj Narayan Prasad <yashraj191220@gmail.com>
2026-08-30 11:26:57 +0530

    Time: 12 ms (37.52%) | Memory: 52.7 MB (76.21%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,15 +3,11 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) + 1);
-            }else{
-                map.put(nums[i],1);
-            }
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
         }
         int result = 0;
-        Set<Integer> keySet = map.keySet();
-        for(Integer key : keySet){
+       
+        for(Integer key : map.keySet()){
             if(map.get(key) > nums.length/2){
                 result = key;
             }
